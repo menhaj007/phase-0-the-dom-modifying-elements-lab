5cd28c31e5b9181810d87803de5d18f5b22e702f
menhaj <menhaj007@yahoo.com>
2021-03-13 18:34:05 -0500

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Write your code here!
+const mainId = document.querySelector("#main").remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "John is the champion";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const element = document.createElement('div');
+// document.body.appendChild(element);
+
+// const ul = document.createElement('ul');
+
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement('li');
+//     li.innerHTML = (i + 1).toString();
+//     ul.appendChild(li);
+// }
+// element.appendChild(ul);
+
+// const header = document.getElementById('header');
+// header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+
+
+// element.style.backgroundColor = '#27647B';
+
+// someElement.removeChild(someChildElement);
+
+// const ul = document.getElementsbyTagName('ul')[0];
+// ul.removeChild(ul.querySelector('li:nth-child(2)'));
+
+// ul.remove();
